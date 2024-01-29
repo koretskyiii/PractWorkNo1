3219c0d1fb99da8f7cc5649566c83c94c00bd8ff
Koretskyi_Yura <ipz221_kyui@student.ztu.edu.ua>
2024-01-29 21:06:41 +0200

    Sum 2 numbers

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SumTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number: ");
+            float firstNum = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            float secondNum = float.Parse(Console.ReadLine());
+            Console.WriteLine($"Sum of numbers = {firstNum + secondNum}");
+        }
+    }
+}
+
